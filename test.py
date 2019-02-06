7ccbd1e49c583ea8b31e2822d63bfcace11dba6a
Emilien Klein <emilien@klein.st>
2019-02-06 22:54:34 +0100

    Initial unittest (for the argument parsing)

---
+++ test.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# https://realpython.com/python-testing/
+# Running the tests:
+# $ python -m unittest -v test
+
+import unittest
+import sys
+sys.path.append('.')
+target = __import__("actioncam-upload")
+import logging
+
+class TestArgparse(unittest.TestCase):
+    def test_arg_dry_run(self):
+        """
+        Test the --dry-run argument
+        """
+        parser = target.parse_args(['--dry-run'])
+        self.assertTrue(parser.dry_run)
+
+    def test_arg_dry_run_shorthand(self):
+        """
+        Test the -dr argument
+        """
+        parser = target.parse_args(['-dr'])
+        self.assertTrue(parser.dry_run)
+
+    def test_arg_no_net(self):
+        """
+        Test the --no-net argument
+        """
+        parser = target.parse_args(['--no-net'])
+        self.assertTrue(parser.no_net)
+
+    def test_arg_no_net_shorthand(self):
+        """
+        Test the -nn argument
+        """
+        parser = target.parse_args(['-nn'])
+        self.assertTrue(parser.no_net)
+
+    def test_arg_debug(self):
+        """
+        Test the --debug argument
+        """
+        parser = target.parse_args(['--debug'])
+        self.assertEqual(parser.loglevel, logging.DEBUG)
+
+    def test_arg_debug_shorthand(self):
+        """
+        Test the -d argument
+        """
+        parser = target.parse_args(['-d'])
+        self.assertEqual(parser.loglevel, logging.DEBUG)
+
+    def test_arg_verbose(self):
+        """
+        Test the --verbose argument
+        """
+        parser = target.parse_args(['--verbose'])
+        self.assertEqual(parser.loglevel, logging.INFO)
+
+    def test_arg_verbose_shorthand(self):
+        """
+        Test the -v argument
+        """
+        parser = target.parse_args(['-v'])
+        self.assertEqual(parser.loglevel, logging.INFO)
+
+    def test_arg_privacyStatus_valid(self):
+        """
+        Test the --privacyStatus argument with a valid value
+        """
+        parser = target.parse_args(['--privacyStatus', 'private'])
+        self.assertEqual(parser.privacyStatus, 'private')
+
+if __name__ == '__main__':
+    unittest.main()
